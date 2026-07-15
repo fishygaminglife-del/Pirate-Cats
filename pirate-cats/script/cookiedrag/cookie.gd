9a8c9a62f8e69326a3ee5600e4ad1100f631ddb5
Fishyyyyyyyyy <justvishy.v@gmail.com>
2026-07-15 14:13:58 -0500

    Start Parrot Feeding
    I have started the parrot feeding, and now you can feed the parrot and you have to wait for it to finish eating to feed it again.

---
+++ pirate-cats/script/cookiedrag/cookie.gd
@@ -0,0 +1,28 @@
+extends Sprite2D
+var dragging = false
+var of = Vector2(0,0)
+var can_drag = true
+var in_zone = false
+
+func _process(delta: float) -> void:
+	if dragging:
+		global_position = get_global_mouse_position() - of
+	
+
+
+func _on_button_button_up() -> void:
+	if can_drag:
+		dragging = false
+		if $Panel2.get_global_rect().intersects($"../Panel".get_global_rect()):
+			if $"..".eating == false:
+				can_drag = false
+				$".".visible = false
+				$"..".parrot_eat()
+			else:
+				$".".global_position = Vector2(-37, 96)
+
+func _on_button_button_down() -> void:
+	if can_drag:
+		dragging = true
+	of = get_global_mouse_position() - global_position
+	
---
+++ pirate-cats/script/parrot.gd
@@ -0,0 +1,19 @@
+extends Node2D
+var eating = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func parrot_eat():
+	if eating == false:
+		eating = true
+		$Parrot.play("eating")
+		await get_tree().create_timer(3).timeout
+		$Parrot.play("default")
+		eating = false
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
