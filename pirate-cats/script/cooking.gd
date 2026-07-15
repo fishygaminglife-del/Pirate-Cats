ef9945cef662d2639d9d2d46ffe782b8dba2b31c
Fishyyyyyyyyy <justvishy.v@gmail.com>
2026-07-14 21:56:20 -0500

    Add Cook Check
    I added a check, so now it checks whether or not if the user made the right order, this is not visual yet.

--- pirate-cats/script/cooking.gd
+++ pirate-cats/script/cooking.gd
@@ -11,6 +11,31 @@ var plate = 0
 var plate_anim = false
 var base
 var top
+#CheckOrder
+var OBase1 = false
+var OBase2 = false
+var OTop1 = false
+var OTop2 = false
+var OShell = false
+var OFlag = false
+var OStar = false
+#OnPlate
+var PBase1 = false
+var PBase2 = false
+var PTop1 = false
+var PTop2 = false
+var PShell = false
+var PFlag = false
+var PStar = false
+# Correct Items
+var CBase1 = false
+var CBase2 = false
+var CTop1 = false
+var CTop2 = false
+var CShell = false
+var CFlag = false
+var CStar = false
+
 func _ready() -> void:
 	$Camera2D.position = Vector2(299,463)
 	$MiddlePlate.self_modulate = Color(1.0, 1.0, 1.0, 0.0)
@@ -39,8 +64,28 @@ func _process(delta: float) -> void:
 	elif plate == 7:
 		$Plates/Plate7.visible = false
 
-		
 func order():
+	OTop1 = false
+	OTop2 = false
+	OShell = false
+	OFlag = false
+	OStar = false
+		#OnPlate
+	PBase1 = false
+	PBase2 = false
+	PTop1 = false
+	PTop2 = false
+	PShell = false
+	PFlag = false
+	PStar = false
+	# Correct Items
+	CBase1 = false
+	CBase2 = false
+	CTop1 = false
+	CTop2 = false
+	CShell = false
+	CFlag = false
+	CStar = false
 	print("works")
 	layer1 = randi_range(1,2)
 	layer2 = randi_range(1,2)
@@ -57,24 +102,29 @@ func order():
 		if lone == 1:
 			$Order/Bottom1.visible = true
 			$Order/Bottom2.visible = false
+			OBase1 = true
 		else:
 			$Order/Bottom1.visible = false
 			$Order/Bottom2.visible = true
+			OBase2 = true
 			
 	if layer2 == 2:
 		ltwo = randi_range(1,2)
 		if ltwo == 1:
+			OTop1 = true
 			if layer1 == 2:
 				$Order/Top2.visible = true
 			else:
 				$"Order/Top2-1".visible = true
 		else:
+			CTop2 = true
 			if layer1 == 2:
 				$Order/Top1.visible = true
 			else:
 				$"Order/Top1-1".visible = true
 
 	if star == 2:
+		OStar = true
 		if layer1 == 2:
 			if $Order/Bottom1.visible == true:
 				$Order/SmallBase/Star.visible = true
@@ -85,6 +135,7 @@ func order():
 		else:
 			$Order/NoBase/Star6.visible = true
 	if shell == 2:
+		OShell = true
 		if layer1 == 2:
 			if $Order/Bottom1.visible == true:
 				$Order/SmallBase/Clam.visible = true
@@ -95,17 +146,59 @@ func order():
 		else:
 			$Order/NoBase/Clam5.visible = true
 	if flag == 2:
+		OFlag = true
 		if layer2 == 2:
 			if layer1 == 2:
-				$Order/Flag3.visible = true
+				$Order/Flag1.visible = true
 			else:
 				$Order/Flag2.visible = true
 		else:
-			$Order/Flag1.visible = true
+			$Order/Flag3.visible = true
 			
 func plate_check():
-	pass
+	if $MiddlePlate/Bottom1.visible == true:
+		PBase1 = true
+	if $MiddlePlate/Bottom2.visible == true:
+		PBase2 = true
+	if $MiddlePlate/Top1.visible == true or $"MiddlePlate/Top1-1".visible == true:
+		PTop1 = true
+	if $MiddlePlate/Top2.visible == true or $"MiddlePlate/Top2-1".visible == true:
+		PTop2 = true
+	if $MiddlePlate/SmallBase/Clam.visible == true or $MiddlePlate/BigBase/Clam3.visible == true or $MiddlePlate/NoBase/Clam5.visible == true:
+		PShell = true
+	if $MiddlePlate/SmallBase/Star.visible == true or $MiddlePlate/BigBase/Star3.visible == true or $MiddlePlate/NoBase/Star6.visible == true:
+		PStar = true
+	if $MiddlePlate/Flag1.visible == true or $MiddlePlate/Flag2.visible == true or $MiddlePlate/Flag3.visible == true:
+		PFlag = true
+	
+	if OBase1 == PBase1:
+		CBase1 = true
+	if OBase2 == PBase2:
+		CBase2 = true
+	if OTop1 == PTop1:
+		CTop1 = true
+	if OTop2 == PTop2:
+		CTop2 = true
+	if OShell == PShell:
+		CShell = true
+	if OStar == PStar:
+		CStar = true
+	if OFlag == PFlag:
+		CFlag  = true
+	
+
+
 func _on_buttonmove_pressed() -> void:
 	$AnimationPlayer.play("CastleFinish")
 	await get_tree().create_timer(0.65).timeout
 	$AnimationPlayer2.play("CastleMenu")
+
+func _on_buttonmove_1_pressed() -> void:
+	plate_check()
+	print(CBase1)
+	print(CBase2 )
+	print(CTop1)
+	print(CTop2 )
+	print(CShell)
+	print(CFlag )
+	print(CStar)
