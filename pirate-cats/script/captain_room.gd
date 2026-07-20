61b61ac43ca9770165682a237f918ff51c57ef2c
Fishyyyyyyyyy <justvishy.v@gmail.com>
2026-07-20 09:59:18 -0500

    Map Drag
    Currently the map is draggable and goes to the last spot you put it on the table (there is an area for map)

--- pirate-cats/script/captain_room.gd
+++ pirate-cats/script/captain_room.gd
@@ -11,12 +11,31 @@ var map3 = false
 var map4 = false
 var map5 = false
 var map6 = false
-var panels = [$PH1, $PH2, $PH3, $PH4, $PH5, $PH6]
-var maps = [$Map1, $Map2, $Map3, $Map4, $Map5, $Map6]
+var pan1 = false
+var pan2 = false
+var pan3 = false
+var pan4 = false
+var pan5 = false
+var pan6 = false
 @onready var butmap = $Map1
-var dragging = false
-var of = Vector2(0,0)
-var in_zone = false
+var dragging1 = false
+var of1 = Vector2(0,0)
+var in_zone1 = false
+var dragging2 = false
+var of2 = Vector2(0,0)
+var in_zone2 = false
+var dragging3 = false
+var of3 = Vector2(0,0)
+var in_zone3 = false
+var dragging4 = false
+var of4 = Vector2(0,0)
+var in_zone4 = false
+var dragging5 = false
+var of5 = Vector2(0,0)
+var in_zone5 = false
+var dragging6 = false
+var of6 = Vector2(0,0)
+var in_zone6 = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,36 +43,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if dragging:
-		$Map1.global_position = get_global_mouse_position() - of
-	if $PH1.get_global_rect().intersects(butmap.get_global_rect()):
-		if $Map1 == butmap:
-			map1 = true
-		if $Map2 == butmap:
-			map2 = true
-		if $Map3 == butmap:
-			map3 = true
-		if $Map4 == butmap:
-			map4 = true
-		if $Map5 == butmap:
-			map5 = true
-		if $Map6 == butmap:
-			map6 = true
-		print(butmap)
+	if dragging1:
+		$Map1.global_position = get_global_mouse_position() - of1
+	if dragging2:
+		$Map2.global_position = get_global_mouse_position() - of2
+	if dragging3:
+		$Map3.global_position = get_global_mouse_position() - of3
+	if dragging4:
+		$Map4.global_position = get_global_mouse_position() - of4
+	if dragging5:
+		$Map5.global_position = get_global_mouse_position() - of5
+	if dragging6:
+		$Map6.global_position = get_global_mouse_position() - of6
 
-	else:
-		if $Map1 == butmap:
-			map1 = false
-		if $Map2 == butmap:
-			map2 = false
-		if $Map3 == butmap:
-			map3 = false
-		if $Map4 == butmap:
-			map4 = false
-		if $Map5 == butmap:
-			map5 = false
-		if $Map6 == butmap:
-			map6 = false
+
+		
 		
 	if moveleft == true:
 		$Camera2D.global_position += Vector2(-150, 0) * delta
@@ -140,9 +144,152 @@ func _on_area_m_3_mouse_entered() -> void:
 
 
 func _on_map_1_button_down() -> void:
-	dragging = true
-	of = get_global_mouse_position() - $Map1.global_position
-
+	dragging1 = true
+	of1 = get_global_mouse_position() - $Map1.global_position
+	butmap = $Map1
 func _on_map_1_button_up() -> void:
-	dragging = false
-	
+	dragging1 = false
+
+	if $Map1.get_global_rect().intersects($MPArea.get_global_rect()):
+		var placeholders = [$PH1, $PH2, $PH3, $PH4, $PH5, $PH6]
+
+		var closest = null
+		var closest_dist = INF
+
+		for ph in placeholders:
+			var dist = $Map1.global_position.distance_to(ph.global_position)
+
+			if dist < closest_dist:
+				closest_dist = dist
+				closest = ph
+
+		if closest:
+				var panel_center = closest.global_position + (closest.size / 2)
+				var map_pos = panel_center - ($Map1.size / 2)
+
+				$Map1.global_position = map_pos
+func _on_map_2_button_down() -> void:
+	dragging2 = true
+	of2 = get_global_mouse_position() - $Map2.global_position
+	butmap = $Map2
+func _on_map_2_button_up() -> void:
+	dragging2 = false
+
+	if $Map2.get_global_rect().intersects($MPArea.get_global_rect()):
+		var placeholders = [$PH1, $PH2, $PH3, $PH4, $PH5, $PH6]
+
+		var closest = null
+		var closest_dist = INF
+
+		for ph in placeholders:
+			var dist = $Map2.global_position.distance_to(ph.global_position)
+
+			if dist < closest_dist:
+				closest_dist = dist
+				closest = ph
+
+		if closest:
+				var panel_center = closest.global_position + (closest.size / 2)
+				var map_pos = panel_center - ($Map2.size / 2)
+
+				$Map2.global_position = map_pos
+func _on_map_3_button_down() -> void:
+	dragging3 = true
+	of3 = get_global_mouse_position() - $Map3.global_position
+	butmap = $Map3
+func _on_map_3_button_up() -> void:
+	dragging3 = false
+
+	if $Map3.get_global_rect().intersects($MPArea.get_global_rect()):
+		var placeholders = [$PH1, $PH2, $PH3, $PH4, $PH5, $PH6]
+
+		var closest = null
+		var closest_dist = INF
+
+		for ph in placeholders:
+			var dist = $Map3.global_position.distance_to(ph.global_position)
+
+			if dist < closest_dist:
+				closest_dist = dist
+				closest = ph
+
+		if closest:
+				var panel_center = closest.global_position + (closest.size / 2)
+				var map_pos = panel_center - ($Map3.size / 2)
+
+				$Map3.global_position = map_pos
+func _on_map_4_button_down() -> void:
+	dragging4 = true
+	of4 = get_global_mouse_position() - $Map4.global_position
+	butmap = $Map4
+func _on_map_4_button_up() -> void:
+	dragging4 = false
+
+	if $Map4.get_global_rect().intersects($MPArea.get_global_rect()):
+		var placeholders = [$PH1, $PH2, $PH3, $PH4, $PH5, $PH6]
+
+		var closest = null
+		var closest_dist = INF
+
+		for ph in placeholders:
+			var dist = $Map4.global_position.distance_to(ph.global_position)
+
+			if dist < closest_dist:
+				closest_dist = dist
+				closest = ph
+
+		if closest:
+				var panel_center = closest.global_position + (closest.size / 2)
+				var map_pos = panel_center - ($Map4.size / 2)
+
+				$Map4.global_position = map_pos
+func _on_map_5_button_down() -> void:
+	dragging5 = true
+	of5 = get_global_mouse_position() - $Map5.global_position
+	butmap = $Map5
+func _on_map_5_button_up() -> void:
+	dragging5 = false
+
+	if $Map5.get_global_rect().intersects($MPArea.get_global_rect()):
+		var placeholders = [$PH1, $PH2, $PH3, $PH4, $PH5, $PH6]
+
+		var closest = null
+		var closest_dist = INF
+
+		for ph in placeholders:
+			var dist = $Map5.global_position.distance_to(ph.global_position)
+
+			if dist < closest_dist:
+				closest_dist = dist
+				closest = ph
+
+		if closest:
+				var panel_center = closest.global_position + (closest.size / 2)
+				var map_pos = panel_center - ($Map5.size / 2)
+
+				$Map5.global_position = map_pos
+func _on_map_6_button_down() -> void:
+	dragging6 = true
+	of6 = get_global_mouse_position() - $Map6.global_position
+	butmap = $Map6
+func _on_map_6_button_up() -> void:
+	dragging6 = false
+
+	if $Map6.get_global_rect().intersects($MPArea.get_global_rect()):
+		var placeholders = [$PH1, $PH2, $PH3, $PH4, $PH5, $PH6]
+
+		var closest = null
+		var closest_dist = INF
+
+		for ph in placeholders:
+			var dist = $Map6.global_position.distance_to(ph.global_position)
+
+			if dist < closest_dist:
+				closest_dist = dist
+				closest = ph
+
+		if closest:
+				var panel_center = closest.global_position + (closest.size / 2)
+				var map_pos = panel_center - ($Map6.size / 2)
+
+				$Map6.global_position = map_pos
