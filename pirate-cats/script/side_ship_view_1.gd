7b2d53c8787bae1b96a0909528bba3a55a3a7cd8
Fishyyyyyyyyy <justvishy.v@gmail.com>
2026-07-21 12:50:49 -0500

    Camera Move + Flag Raise
    I added to code to make the camera  move where your mouse is facing, and now i have the flag animation, and have made it that when hovering for the second time it puts animation in reverse (flag coming down).

--- pirate-cats/script/side_ship_view_1.gd
+++ pirate-cats/script/side_ship_view_1.gd
@@ -1,35 +1,73 @@
 extends Node2D
-
-
+var moveleft = false
+var moveright = false
+var flag = 0
+var animationf_play = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	if moveleft == true:
+		$Camera2D.global_position += Vector2(-150, 0) * delta
+	$Camera2D.global_position.x = clamp(
+		$Camera2D.global_position.x,
+		288,
+		813
+	)
+	if moveright == true:
+		$Camera2D.global_position += Vector2(150, 0) * delta
+	$Camera2D.global_position.x = clamp(
+		$Camera2D.global_position.x,
+		288,
+		813
+	)
 
 func _on_crab_out_mouse_entered() -> void:
 	$AnimatedSprite2D.play("default")
 
-
 func _on_crab_out_mouse_exited() -> void:
 	$AnimatedSprite2D.stop()
-
 
 func _on_dyna_play_mouse_entered() -> void:
 	$Dynamite.play("default")
 
-
 func _on_dyna_play_mouse_exited() -> void:
 	$Dynamite.stop()
-
 
 func _on_bouy_play_mouse_entered() -> void:
 	$Bouy.play("default")
 
-
 func _on_bouy_play_mouse_exited() -> void:
 	$Bouy.stop()
+
+
+func _on_cam_left_mouse_entered() -> void:
+	moveleft = true
+
+
+func _on_cam_left_mouse_exited() -> void:
+	moveleft = false
+
+
+func _on_cam_right_mouse_entered() -> void:
+	moveright = true
+
+func _on_cam_right_mouse_exited() -> void:
+	moveright = false
+
+
+func _on_flag_raise_mouse_entered() -> void:
+	if animationf_play == false:
+		animationf_play = true
+		flag = 1
+		$AnimationPlayer.play("FlagRaise")
+	elif flag == 0:
+		flag = 1
+		$AnimationPlayer.play("FlagRaise", -1, 1)
+	else:
+		flag = 0
+		$AnimationPlayer.play("FlagRaise", -1, -1)
+	
+		
