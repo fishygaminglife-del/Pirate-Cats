28690625b5cc324af0ee75429cd9185168ea0d88
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2026-07-24 18:53:50 -0500

    start dialogue manager in scene 1

--- pirate-cats/script/ship_sailing.gd
+++ pirate-cats/script/ship_sailing.gd
@@ -4,6 +4,42 @@ var you_text = [
 	"...",
 	"Where am I?",
 ]
+var dialogue = [
+	{
+		"speaker": "you",
+		"name": "",
+		"text": "..."
+	},
+	{
+		"speaker": "you",
+		"name": "",
+		"text": "Where am I?"
+	},
+	{
+		"speaker": "cat",
+		"name": "???",
+		"text": "Hey."
+	},
+	{
+		"speaker": "cat",
+		"name": "???",
+		"text": "You're finally awake."
+	},
+]
+
+var dialogue_index = 0
+var typing = false
+#var cat_dialogue = [
+	#{
+		#"name": "???",
+		#"text": "Hey.",
+	#},
+	##{
+		##"name": "???",
+		##"text": "You're finally awake.",
+		##"portrait": preload("res://assets/headshots/POWDER MONKEY FACE.png")
+	##},
+#]
 var you_text_index = 0
 
 func _ready() -> void:
@@ -17,7 +53,47 @@ func _ready() -> void:
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/loading.tscn")
 
+		
+func show_next_dialogue() -> void:
+	if dialogue_index >= dialogue.size():
+		$Textbox.visible = false
+		$Button.visible = false
+		return
+	
+	var line = dialogue[dialogue_index]
+	
+	if line["speaker"] == "you":
+		$Textbox.visible = false
+		$Button.visible = true
+		$Button.text = line["text"]
+		
+	elif line["speaker"] == "cat":
+		$Button.visible = false
+		show_cat_text(line)
+
 
 func _on_player_button_pressed() -> void:
-	you_text_index +=1
-	$Button.text = you_text[you_text_index]
+	dialogue_index += 1
+	show_next_dialogue()
+
+
+func show_cat_text(line) -> void:
+	$Textbox.visible = true
+	typing = true
+	
+	$Textbox/namelabel.text = line["name"]
+	$Textbox/textlabel.text = line["text"]
+	$Textbox/textlabel.visible_characters = 0
+	
+	for i in $Textbox/textlabel.text.length():
+		$Textbox/textlabel.visible_characters = i
+		await get_tree().create_timer(0.05).timeout
+	
+	typing = false
+
+
+func _input(event):
+	if event.is_action_pressed("ui_accept") and !typing:
+		if dialogue_index < dialogue.size() and dialogue[dialogue_index]["speaker"] == "cat":
+			dialogue_index += 1
+			show_next_dialogue()
